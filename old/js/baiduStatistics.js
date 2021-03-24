6e23a6b68279a0838df977f7646bfd14d8464b17
Yao Zhou <zhouyao@mail2.sysu.edu.cn>
2021-03-24 17:29:07 +0800

    Delete old directory

--- old/js/baiduStatistics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-			(function() {
- 				var hm = document.createElement("script");
-  					hm.src = "//hm.baidu.com/hm.js?887f2cb612ca4c821456b587231e77fd";
-  				var s = document.getElementsByTagName("script")[0]; 
- 				 s.parentNode.insertBefore(hm, s);
-			})();
